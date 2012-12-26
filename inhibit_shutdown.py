aa4ae5b722b24f64618a38db783128e6f077d38c
Kuba Marek <blue.cube@seznam.cz>
2012-12-26 03:05:59 +0100

    Configurable, fixed a bug on some kernels.

--- inhibit_shutdown.py
+++ inhibit_shutdown.py
@@ -2,37 +2,60 @@
 from __future__ import print_function
 
 import xbmc
+import xbmcaddon
 import subprocess
+import re
 
-# Set of (protocol, local port) tuples.
-watched = {
-    ('tcp', 22), # SSH
-    ('tcp', 445), # samba
-    }
-sleep_time = 60 * 1000 # sleep time between checks in miliseconds
+def port_set(string):
+    ret = set()
+    for port in re.findall("[0-9]+", string):
+        try:
+            port = int(port)
+        except ValueError:
+            continue
+        ret.add(port)
+    return ret
 
 def log(msg):
-    print("service.inhibit_shutdown: {}".format(msg))
+    print("{}: {}".format(addon.getAddonInfo('id'), msg))
 
 def check_services():
     """ Check if any of the watched services is running. """
 
-    netstat = subprocess.check_output(['/bin/netstat', '--protocol=inet', '-n'], universal_newlines=True)
+    netstat = subprocess.check_output(['/bin/netstat', '-t', '-n'], universal_newlines=True)
 
     for line in netstat.split('\n')[2:]:
         items = line.split()
         if len(items) < 4:
             continue
 
-        proto = items[0]
-        port = int(items[3].split(':')[-1])
+        local_addr, local_port = items[3].split(':')
+        remote_addr, remote_port = items[4].split(':')
 
-        if (proto, port) in watched:
-            log("Found {} connection from {} to port {}".format(proto, items[4], port))
+        local_port = int(local_port)
+
+        if ((local_addr != remote_addr) and (local_port in watched_remote)) or \
+            ((local_addr == remote_addr) and (local_port in watched_local)):
+            log("Found connection from {} to {}:{}".format(remote_addr, local_addr, local_port))
             return True
 
     log("No connection found.")
     return False
+
+
+addon = xbmcaddon.Addon()
+s = addon.getSetting
+try:
+    sleep_time = float(s('sleep')) * 1000
+except ValueError:
+    sleep_time = 60 * 1000
+watched_local = port_set(s('localports'))
+watched_remote = port_set(s('remoteports'))
+
+log("Watching for remote connections to ports {} and for local connections to ports {}, sleep time is {:0.2f} s.".format(
+    ', '.join(str(x) for x in watched_remote),
+    ', '.join(str(x) for x in watched_local),
+    sleep_time / 1000.0))
 
 while not xbmc.abortRequested:
     if check_services():
